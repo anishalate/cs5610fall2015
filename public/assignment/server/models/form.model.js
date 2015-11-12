d4607ab573edf92ff8ee1d4c39f43682895cddde
Anish Alate <anish.alate@gmail.com>
2015-11-12 02:19:28 -0500

    Created form service and model

--- public/assignment/server/models/form.model.js
+++ public/assignment/server/models/form.model.js
@@ -1,3 +1,88 @@
-/**
- * Created by Anish on 11/11/2015.
- */
+var forms = require("./form.mock.json");
+
+module.exports = function(app) {
+
+    var api = {
+        createForm: createForm,
+        findAll: findAll,
+        findById: findById,
+        updateForm: updateForm,
+        deleteForm: deleteForm,
+        findFormByTitle: findFormByTitle,
+        findFormByUserId: findFormByUser,
+
+
+    };
+
+    return api;
+    function createForm(form){
+        forms.push(form);
+        return forms;
+    }
+
+    function findAll(){
+        return forms;
+
+    }
+
+    function findById(id){
+        var formById={};
+        for(var i=0;i<forms.length;i++)
+        {
+            if(forms[i].id==id){
+                formById = forms[i];
+                break;
+            }
+        }
+        return formById;
+
+    }
+
+    function updateForm(id,updatedForm){
+        for(var i=0;i<forms.length;i++)
+        {
+            if(forms[i].id==id){
+                updatedForm.id = id;
+                forms[i] = updatedForm;
+                break;
+            }
+        }
+        return forms;
+
+    }
+    function deleteForm(id){
+        for(var i=0;i<forms.length;i++)
+        {
+            if(forms[i].id==id){
+                forms.splice(i,1);
+                break;
+            }
+        }
+        return forms;
+
+    }
+    function findFormByTitle(title){
+        var formByTitle={};
+        for(var i=0;i<forms.length;i++)
+        {
+            if(forms[i].title==title){
+                formByTitle = forms[i];
+                break;
+            }
+        }
+        return formByTitle;
+
+    }
+
+    function findFormByUser(userId){
+        var formsByUser = []
+        for(var i=0;i<forms.length;i++)
+        {
+            if(forms[i].userId==userId){
+                formsByUser.push(forms[i]);
+            }
+        }
+        return formsByUser;
+    }
+
+}
--- public/assignment/server/services/form.service.js
+++ public/assignment/server/services/form.service.js
@@ -1,3 +1,35 @@
-/**
- * Created by Anish on 11/11/2015.
- */
+module.exports = function(app,model){
+
+   app.get("/api/assignment/user/:userId/form",findFormByUser)
+   app.get("/api/assignment/form/:formId", getFormById);
+   app.delete("/api/assignment/form/:formId",deleteForm);
+   app.post("/api/assignment/user:userId/form",createForm);
+   app.put("/api/assignment/form/:formId",updateForm);
+
+
+    function getFormById(req,res){
+        res.json(model.findById(req.params.formId));
+    }
+
+    function findFormByUser(req,res){
+        res.json(model.findFormByUserId(req.params.userId));
+
+    }
+
+    function deleteForm(req,res){
+        res.json(model.deleteForm(req.params.formId));
+
+    }
+
+    function createForm(req,res){
+        var form = req.body;
+        res.json(model.createForm(form));
+
+    }
+    function updateForm(req,res){
+        var form = req.body;
+        res.json(model.updateForm(req.params.formId,form));
+
+    }
+
+}
