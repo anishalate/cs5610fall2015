bd698f011a776401a5dae5c23e3da3747ef933c1
Anish Alate <anish.alate@gmail.com>
2015-11-28 20:32:57 -0500

    Integrated AWS api for storing profile pictures

--- public/project/client/views/user-profile/user-profile.controller.js
+++ public/project/client/views/user-profile/user-profile.controller.js
@@ -16,20 +16,115 @@
         $scope.food1="";
         $scope.user={};
         $scope.editContactInfo =true;
+        $scope.showUpload=false;
+        $scope.sizeLimit      = 5292880; //5MB in Bytes
+        $scope.uploadProgress = 0;
+        $scope.creds = {
+            bucket: 'cs5610anish',
+            access_key: 'AKIAJNX74V2SPUBGSRLQ',
+            secret_key: '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'
+        };
+        $scope.profilePicUrl = "http://www.gravatar.com/avatar/";
         init();
 
         function init(){
             if($rootScope.currentUser!==undefined){
                 $scope.user= $rootScope.currentUser;
+                AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
+                AWS.config.region = 'us-east-1';
+                if($scope.user.userDetails.profilePicUrl!==undefined){
+                    generatePicUrl();
+
+                }
             }
             else{
              $location.path("/signin");
             }
         }
 
+        function generatePicUrl(){
+            var s3 = new AWS.S3();
+            var params = {Bucket: $scope.creds.bucket, Key:$scope.user.userDetails.profilePicUrl , Expires: 60};
+            var url = s3.getSignedUrl('getObject', params, function (err, url) {
+                if (url){
+                    console.log("The URL is", url);
+                    $scope.profilePicUrl = url;
+                }
+            });
+
+        }
         $scope.edit=function(){
             $scope.showEdit = true;
             $scope.showDisplay = false;
+
+        }
+
+        $scope.editPic =function(){
+            $scope.showUpload=true;
+        }
+
+        $scope.uploadPic = function(){
+            var selectedFile = document.getElementById('imgFile').files[0];
+            console.log(selectedFile.name + selectedFile.size);
+
+
+            var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
+
+            if(selectedFile) {
+                // Perform File Size Check First
+                var fileSize = Math.round(parseInt(selectedFile.size));
+                if (fileSize > $scope.sizeLimit) {
+                    alert("Please select files less than 5MB")
+                    return false;
+                }
+                // Prepend Unique String To Prevent Overwrites
+                var uniqueFileName = $scope.user._id;
+
+                var params = { Key: uniqueFileName, ContentType: selectedFile.type, Body: selectedFile,ACL:'public-read' };
+               /* if($scope.user.userDetails.profilePicUrl!==undefined){
+                    bucket.deleteObject({Key:$scope.user.userDetails.profilePicUrl},function(err,data){
+
+                    });
+                }*/
+
+                bucket.putObject(params, function(err, data) {
+                    if(err) {
+                        alert(err);
+                        return false;
+                    }
+                    else {
+                        generatePicUrl();
+                        // Upload Successfully Finished
+                        // Reset The Progress Bar
+                        setTimeout(function() {
+                            $scope.uploadProgress = 0;
+                            $scope.$digest();
+                        }, 4000);
+                    }
+                })
+                    .on('httpUploadProgress',function(progress) {
+                        $scope.uploadProgress = Math.round(progress.loaded / progress.total * 100);
+                        $scope.$digest();
+                        if($scope.user.userDetails.profilePicUrl != uniqueFileName) {
+                            $scope.user.userDetails.profilePicUrl != uniqueFileName;
+                            UserService.updateUser($scope.user._id, $scope.user)
+                                .then(function (user) {
+
+                                });
+                        }
+
+
+                    });
+            }
+            else {
+                // No File Selected
+                alert('Please select a file to upload');
+            }
+
+
+
+
+            $scope.showUpload= false;
 
         }
         $scope.updatePref =function(){
@@ -43,6 +138,7 @@
             console.log($scope.user);
             UserService.updateUser($scope.user._id,$scope.user)
                 .then(function(user){
+
 
                 })
         }
